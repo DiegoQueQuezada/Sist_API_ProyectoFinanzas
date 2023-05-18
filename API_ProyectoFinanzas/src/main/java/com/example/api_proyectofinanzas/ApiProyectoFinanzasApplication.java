1115c401115fa74bbc2fdcb77402437c29dc340a
Diego Quezada <diegodavid0404@hotmail.com>
2023-05-17 22:55:02 -0500

    Primer commit del proyecto

---
+++ API_ProyectoFinanzas/src/main/java/com/example/api_proyectofinanzas/ApiProyectoFinanzasApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_proyectofinanzas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProyectoFinanzasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiProyectoFinanzasApplication.class, args);
+    }
+
+}
